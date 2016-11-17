08c27a515192cfe154e699d13620784f2e4399da
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-11-17 23:33:23 +0900

    JankenLogicTestを修正

--- src/com/github/sekiguchikai/model/JankenLogicTest.java
+++ src/com/github/sekiguchikai/model/JankenLogicTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class JankenLogicTest {
 	private final int GU = 0;
-	private final int THOKI = 1;
+	private final int TYOKI = 1;
 	private final int PA = 2;
 
 	/**
@@ -17,12 +17,79 @@ public class JankenLogicTest {
 		// 1.Jankenへ打ち手の登録
 		Janken janken = new Janken();
 		janken.setClientUchite(GU);
-		janken.setServerUchite(THOKI);
+		janken.setServerUchite(TYOKI);
 		JankenLogic jankenLogic = new JankenLogic();
 		jankenLogic.judgeResult(janken);
 
 		// 2.JankenLogicの呼び出し
 		assertEquals("クライアントがグー, サーバーがチョキの場合のテスト", "君の勝ちだ", janken.getResult());
+
+		// 1.Jankenへ打ち手の登録
+		Janken janken2 = new Janken();
+		janken2.setClientUchite(TYOKI);
+		janken2.setServerUchite(PA);
+		JankenLogic jankenLogic2 = new JankenLogic();
+		jankenLogic2.judgeResult(janken2);
+
+		// 2.JankenLogicの呼び出し
+		assertEquals("クライアントがチョキ, サーバーがパーの場合のテスト", "君の勝ちだ", janken2.getResult());
+
+		// 1.Jankenへ打ち手の登録
+		Janken janken3 = new Janken();
+		janken3.setClientUchite(PA);
+		janken3.setServerUchite(GU);
+		JankenLogic jankenLogic3 = new JankenLogic();
+		jankenLogic3.judgeResult(janken3);
+
+		// 2.JankenLogicの呼び出し
+		assertEquals("クライアントがパー, サーバーがグーの場合のテスト", "君の勝ちだ", janken3.getResult());
+
+		// 1.Jankenへ打ち手の登録
+		Janken janken4 = new Janken();
+		janken4.setClientUchite(TYOKI);
+		janken4.setServerUchite(GU);
+		JankenLogic jankenLogic4 = new JankenLogic();
+		jankenLogic4.judgeResult(janken4);
+
+		// 2.JankenLogicの呼び出し
+		assertEquals("クライアントがチョキ, サーバーがグーの場合のテスト", "君の負けだ", janken4.getResult());
+
+		// 1.Jankenへ打ち手の登録
+		Janken janken5 = new Janken();
+		janken5.setClientUchite(PA);
+		janken5.setServerUchite(TYOKI);
+		JankenLogic jankenLogic5 = new JankenLogic();
+		jankenLogic5.judgeResult(janken5);
+
+		// 2.JankenLogicの呼び出し
+		assertEquals("クライアントがパー, サーバーがチョキの場合のテスト", "君の負けだ", janken5.getResult());
+
+		// 1.Jankenへ打ち手の登録
+		Janken janken6 = new Janken();
+		janken6.setClientUchite(GU);
+		janken6.setServerUchite(PA);
+		JankenLogic jankenLogic6 = new JankenLogic();
+		jankenLogic6.judgeResult(janken6);
+
+		// 2.JankenLogicの呼び出し
+		assertEquals("クライアントがグー, サーバーがパーの場合のテスト", "君の負けだ", janken6.getResult());
+
+		int uchiteArray[] = { GU, TYOKI, PA };
+
+		for (int i = 0; i < uchiteArray.length; i++) {
+
+			// 1.Jankenへ打ち手の登録
+			Janken janken7 = new Janken();
+			janken7.setClientUchite(uchiteArray[i]);
+			janken7.setServerUchite(uchiteArray[i]);
+			JankenLogic jankenLogic7 = new JankenLogic();
+			jankenLogic7.judgeResult(janken7);
+
+			// 2.JankenLogicの呼び出し
+			assertEquals("クライアントがグー, サーバーがチョキの場合のテスト", "引き分けだ", janken7.getResult());
+
+		}
+
 	}
 
 }
